760895139d813f7c459b874bbea330a3d3f91b99
bmhany <boukhamlahani9@gmail.com>
2026-05-13 21:34:17 +0100

    Delete verify_css.py

--- verify_css.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-url = 'http://127.0.0.1:8000/static/gestion_transport/style.css'
-try:
-    with urllib.request.urlopen(url) as r:
-        print('STATUS', r.status)
-        text = r.read(120).decode('utf-8', errors='replace')
-        print(text)
-except Exception as e:
-    print('ERROR', e)
